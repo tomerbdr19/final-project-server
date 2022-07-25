b254028ad7367beb785263a1efdd32d7a11a0261
tomerbin <tomerbin@wix.com>
2022-07-25 19:15:06 +0300

    chore(server): use new controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,5 @@
 export * from './User';
 export { AuthController } from './Auth.controller';
+export { PostController } from './Post.controller';
+export { SubscribeController } from './Subscribe.controller';
+export { BusinessController } from './Business.controller';
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import 'dotenv/config';
 import { App } from './app';
-import { UserController } from './controllers/User';
-import { AuthController } from './controllers/Auth.controller';
+import {
+    UserController,
+    AuthController,
+    PostController,
+    SubscribeController,
+    BusinessController
+} from './controllers';
 
 const IS_DEV_MODE = true;
 
 const app = new App(
-  Number(process.env.PORT),
-  [new UserController(), new AuthController()],
-  (IS_DEV_MODE
-    ? process.env.DB_CONN_STRING_DEV
-    : process.env.DB_CONN_STRING) as string
+    Number(process.env.PORT),
+    [
+        new UserController(),
+        new AuthController(),
+        new PostController(),
+        new SubscribeController(),
+        new BusinessController()
+    ],
+    (IS_DEV_MODE
+        ? process.env.DB_CONN_STRING_DEV
+        : process.env.DB_CONN_STRING) as string
 );
 
 app.listen();
