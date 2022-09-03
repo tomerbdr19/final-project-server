65a15093a6a9ee1719ab1502a3ee4f53feecbd8e
tomerbin <tomerbin@wix.com>
2022-09-03 18:10:38 +0300

    meh

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,8 @@
-const IS_DEV_MODE = true;
+import 'dotenv/config';
+
+const IS_DEV_MODE = process.env.NODE_ENV === 'dev';
 export const UPLOAD_DIRECTORY_PATH = './uploads/';
 export const QR_CODE_DIRECTORY_PATH = './QRcodes/';
-export const SERVER_URL_BASE = IS_DEV_MODE ? 'http://localhost:3000/' : '';
+export const SERVER_URL_BASE = IS_DEV_MODE
+    ? 'http://localhost:3000/'
+    : 'https://biz-app-server.azurewebsites.net/';
